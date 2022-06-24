6ab5cc81382bd4a851e9d5e2dce95f446e1bcefe
Reza Syahputra Sinambela <reza190180099@mhs.unimal.ac.id>
2022-06-24 11:00:06 +0700

    update dev

--- flutter_bayeue/lib/pages/profile/profilepage.dart
+++ flutter_bayeue/lib/pages/profile/profilepage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
+
 import 'package:unicons/unicons.dart';
-=======
+
 import 'package:flutter_bayeue/pages/login/login_page.dart';
->>>>>>> login-regis
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -11,7 +10,6 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 227, 244, 254),
         elevation: 0,
@@ -307,7 +305,13 @@ class Profile extends StatelessWidget {
               color: const Color.fromRGBO(58, 162, 220, 1),
               child: InkWell(
                 splashColor: Colors.blue.withAlpha(50),
-                onTap: () {},
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const LoginPage(),
+                      ));
+                },
                 child: SizedBox(
                   height: 60,
                   width: 366,
@@ -332,24 +336,6 @@ class Profile extends StatelessWidget {
           ],
         ),
       ),
-=======
-      backgroundColor: const Color.fromARGB(255, 247, 240, 240),
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const LoginPage(),
-                ),
-              );
-            },
-            child: const Text('Log Out'),
-          ),
-        ),
-      ),
->>>>>>> login-regis
     );
   }
 }
