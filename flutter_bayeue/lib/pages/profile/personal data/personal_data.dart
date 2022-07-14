749d2b4af727181a0b66c739e71127de836565c6
Reza Syahputra Sinambela <reza190180099@mhs.unimal.ac.id>
2022-07-14 22:14:27 +0700

    Update Value

--- flutter_bayeue/lib/pages/profile/personal data/personal_data.dart
+++ flutter_bayeue/lib/pages/profile/personal data/personal_data.dart
@@ -13,6 +13,7 @@ class PersonalData extends StatefulWidget {
 }
 
 class _PersonalDataState extends State<PersonalData> {
+  final _key = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _noTelpController = TextEditingController();
@@ -29,7 +30,6 @@ class _PersonalDataState extends State<PersonalData> {
         _nameController.text = accountModel!.result!.user!.name!;
         _emailController.text = accountModel!.result!.user!.email!;
         _noTelpController.text = accountModel!.result!.user!.phone!;
-        _passwordController.text = accountModel!.result!.user!.phone!;
       }
     });
   }
@@ -67,203 +67,216 @@ class _PersonalDataState extends State<PersonalData> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 32),
-            if (accountModel != null)
-              Center(
-                child: Stack(
-                  children: [
-                    authProvider.imgGallery != null
-                        ? Container(
-                            height: 160,
-                            width: 160,
-                            padding: const EdgeInsets.all(20),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
+        child: Form(
+          key: _key,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 32),
+              if (accountModel != null)
+                Center(
+                  child: Stack(
+                    children: [
+                      authProvider.imgGallery != null
+                          ? Container(
+                              height: 160,
+                              width: 160,
+                              padding: const EdgeInsets.all(20),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: FileImage(
+                                            authProvider.imgGallery!)),
+                                    borderRadius: BorderRadius.circular(10),
+                                    shape: BoxShape.rectangle),
+                              ),
+                            )
+                          : Container(
+                              height: 160,
+                              width: 160,
+                              padding: const EdgeInsets.all(20),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
                                       fit: BoxFit.fill,
-                                      image:
-                                          FileImage(authProvider.imgGallery!)),
-                                  borderRadius: BorderRadius.circular(10),
-                                  shape: BoxShape.rectangle),
+                                      image: NetworkImage(
+                                          accountModel!.result!.user!.image!),
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                    shape: BoxShape.rectangle),
+                              ),
                             ),
-                          )
-                        : Container(
-                            height: 160,
-                            width: 160,
-                            padding: const EdgeInsets.all(20),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: NetworkImage(
-                                        accountModel!.result!.user!.image!),
-                                  ),
-                                  borderRadius: BorderRadius.circular(10),
-                                  shape: BoxShape.rectangle),
+                      Positioned(
+                        right: 1,
+                        bottom: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(360),
+                              color: Colors.blue),
+                          child: IconButton(
+                            onPressed: () {
+                              authProvider.changeprofile(
+                                  'nama', 'email', 'password', 'phone', 'img');
+                            },
+                            icon: const Icon(
+                              UniconsLine.camera,
+                              color: Colors.white,
                             ),
-                          ),
-                    Positioned(
-                      right: 1,
-                      bottom: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(360),
-                            color: Colors.blue),
-                        child: IconButton(
-                          onPressed: () {
-                            authProvider.changeprofile(
-                                'nama', 'email', 'password', 'phone', 'img');
-                          },
-                          icon: const Icon(
-                            UniconsLine.camera,
-                            color: Colors.white,
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Nama',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Email',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Phone',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: TextFormField(
-                controller: _noTelpController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'Password',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: TextFormField(
-                controller: _noTelpController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+                    ],
                   ),
                 ),
-                onPressed: () async {
-                  await authProvider.changeData(
-                      _nameController.text,
-                      _emailController.text,
-                      _passwordController.text,
-                      _noTelpController.text);
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'SUBMIT',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              const SizedBox(
+                height: 50,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'Nama',
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'Email',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: TextFormField(
+                  enabled: false,
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'Phone',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: TextFormField(
+                  enabled: false,
+                  controller: _noTelpController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'Password',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Password Tidak Boleh Kosong';
+                    }
+                  },
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Masukan Password',
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () async {
+                    if (_key.currentState!.validate()) {
+                      await authProvider.changeData(
+                          _nameController.text,
+                          _emailController.text,
+                          _passwordController.text,
+                          _noTelpController.text);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text(
+                    'SUBMIT',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
         ),
       ),
     );
